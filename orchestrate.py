b91030f9fc13a95142f7c76b43856829d75c0e14
Abiola Daropale <39301529+olaseth39@users.noreply.github.com>
2026-09-26 15:02:49 +0000

    fix: correct orchestrate.py task commands

--- orchestrate.py
+++ orchestrate.py
@@ -1,7 +1,7 @@
 from prefect import flow, task
 import subprocess
 import os
-import sys 
+import sys
 
 # Define the paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -11,15 +11,13 @@ DBT_DIR = os.path.join(BASE_DIR, "ecommerce_dbt")
 def run_python_ingestion():
     """Task 1: Runs the Python script to ingest data."""
     print("Running Python ingestion...")
-    #subprocess.run([sys.executable, "ingest_api.py"], check=True, cwd=BASE_DIR)
-    subprocess.run([sys.executable, "-m", "dbt", "run"], check=True, cwd=DBT_DIR)
+    subprocess.run([sys.executable, "ingest_api.py"], check=True, cwd=BASE_DIR)
 
 @task
 def run_dbt_transformations():
     """Task 2: Runs dbt models to transform data."""
     print("Running dbt transformations...")
-    #subprocess.run(["dbt", "run"], check=True, cwd=DBT_DIR)
-    subprocess.run([sys.executable, "-m", "dbt", "test"], check=True, cwd=DBT_DIR)
+    subprocess.run(["dbt", "run"], check=True, cwd=DBT_DIR)
 
 @task
 def run_dbt_tests():
@@ -27,10 +25,9 @@ def run_dbt_tests():
     print("Running dbt tests...")
     subprocess.run(["dbt", "test"], check=True, cwd=DBT_DIR)
 
-# Define the Flow (equivalent to an Airflow DAG)
+# Define the Flow
 @flow(name="ecommerce_daily_pipeline")
 def ecommerce_pipeline():
-    # Define the order of execution (Ingest -> Transform -> Test)
     run_python_ingestion()
     run_dbt_transformations()
     run_dbt_tests()
